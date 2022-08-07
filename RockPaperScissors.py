f98f3a72bf7e09c5f61c6f4001c7ea9ba50b51f0
Shivanshu <83326671+Shivanshu-tb@users.noreply.github.com>
2022-08-07 17:37:51 +0530

    Create RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,18 @@
+'''
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+'''
+
+def rps(p1, p2):
+    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if beats[p1] == p2:
+        return "Player 1 won!"
+    if beats[p2] == p1:
+        return "Player 2 won!"
+    return "Draw!"
